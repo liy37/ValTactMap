a1e74241a2fada1b86c12a492f6a5d5907f05b99
liy37 <yuantai.dl@gmail.com>
2020-08-16 17:09:28 -0400

    add event listener

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.querySelectorAll(".MapSelect").forEach(item => {
+    item.addEventListener("click", function () {
+        changeMap(item.id);
+    }, false);
+});
+
 function changeMap(mapName) {
     var currMap = document.getElementById("map")
     currMap.src = "assets/" + mapName + ".svg" 
